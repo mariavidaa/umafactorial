7ea790b3c571502ec1d9bb2037e7bf405219540e
mariavidaa <mariavida262001@uma.es>
2023-02-10 17:39:33 +0100

    refactorizacion

--- factorial.java
+++ factorial.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class factorial {
+
+	public static long fact(long num) {
+		long fac = 1;
+		for (long i=1; i<=num; ++i) {
+			fac = fac * i;
+		}
+		return fac;
+	}
 	public static void main(String []args) {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
