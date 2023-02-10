cedcd2dc7aabcb5a295bde88c4b28794dfa1c473
mariavidaa <mariavida262001@uma.es>
2023-02-10 17:43:50 +0100

    correcion numeros negativos

--- factorial.java
+++ factorial.java
@@ -12,11 +12,13 @@ public class factorial {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
 		long num = scanInput.nextLong();
-		long fac = 1;
-		for (long i=1; i<=num; ++i) {
-			fac = fac * i;
+		if (num<0) {
+			System.out.println("El factorial no esta definido"
+					+ " para numeros negativos");
+		} else {
+			long fac = fact(num);
+			System.out.println("El factorial de "+num+" es "+fac);
 		}
-		System.out.println("El factorial de "+num+" es "+fac);
 		scanInput.close();
 	}
 }
